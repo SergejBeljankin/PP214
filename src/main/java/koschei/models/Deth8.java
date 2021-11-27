b3d8d356e944574ffdab5f97540226ed08168e2d
Belyankin Sergey <beljankin@yandex.ru>
2021-11-27 13:11:19 +0300

    решение 214

--- src/main/java/koschei/models/Deth8.java
+++ src/main/java/koschei/models/Deth8.java
@@ -1,4 +1,19 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Deth8 {
+
+    private String s;
+
+    public Deth8() {
+        this.s = "";
+    }
+
+    @Override
+    public String toString() {
+        return s;
+    }
 }
